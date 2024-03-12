02d15e809c9cdc3c3948db3aa21611908bdc96c7
Ariel M. Cohen <arielco777@gmail.com>
2024-03-12 16:56:11 -0400

    fix

--- src/Home.tsx
+++ src/Home.tsx
@@ -34,83 +34,79 @@ const Home = () => {
             >
                 <div>
                     <p>
-                        I am a full-stack developer in with experience with{" "}
-                        <b>React</b> as front and <b>Python</b> as back. I
-                        graduated as a full-stack in <b>Java</b> and in computer
-                        science with knowledge in <b>C++</b> and <b>C#</b> added
-                        to my stack.
+                        I am a full-stack developer based in Montreal, QC,
+                        Canada.
+                        <br /> I have experience with <b>React</b> as front and{" "}
+                        <b>Java</b>, <b>Node</b>, and <b> Python</b> as back.
                     </p>
                 </div>
             </section>
             <section id="technologies" className="py-10 w-full px-4 border-t">
                 <h2 className="text-center text-2xl pb-5">Web Technologies</h2>
-                <div className="flex lg:flex-row flex-col lg:justify-center w-full gap-3 lg:gap-10 ">
-                    <div className="border rounded-lg min-w-40 ">
+                <div className="flex lg:flex-row flex-col lg:justify-center w-full gap-3 lg:gap-10 lg:min-h-[20vh] ">
+                    <div className="border rounded-lg min-w-40 flex flex-col">
                         <h3 className="text-center font-bold py-1 text-xl bg-neutral-600 text-white rounded-t-lg">
                             Front
                         </h3>
-                        <ul className="flex flex-col gap-1 text-center p-3 ">
+                        <ul className="h-full flex flex-col gap-1 text-center p-3 justify-around">
                             <li>HTML, CSS, JS</li>
                             <li>React, Next</li>
                             <li>Tailwind, BootStrap</li>
                         </ul>
                     </div>
-                    <div className="border rounded-lg min-w-40">
+                    <div className="border rounded-lg min-w-40 flex flex-col">
                         <h3 className="text-center font-bold py-1 text-xl bg-neutral-600 text-white rounded-t-lg">
                             Back
                         </h3>
-                        <ul className="flex flex-col gap-1 text-center p-3 ">
+                        <ul className="h-full flex flex-col gap-1 text-center p-3 justify-around">
                             <li>Java, Spring</li>
                             <li>Python</li>
                             <li>Node</li>
                         </ul>
                     </div>
-                    <div className="border rounded-lg min-w-40">
+                    <div className="border rounded-lg min-w-40 flex flex-col">
                         <h3 className="text-center font-bold py-1 text-xl bg-neutral-600 text-white rounded-t-lg">
                             Databases
                         </h3>
-                        <ul className="flex flex-col gap-1 text-center p-3 ">
+                        <ul className="h-full flex flex-col gap-1 text-center p-3 justify-around">
                             <li>MySQL</li>
                             <li>SQL</li>
                             <li>MongoDB</li>
                         </ul>
                     </div>
-                    <div className="border rounded-lg min-w-40">
+                    {/* <div className="border rounded-lg min-w-40 flex flex-col">
                         <h3 className="text-center font-bold py-1 text-xl bg-neutral-600 text-white rounded-t-lg">
                             Dev Tools
                         </h3>
-                        <ul className="flex flex-col gap-1 text-center p-3 ">
+                        <ul className="h-full flex flex-col gap-1 text-center p-3 justify-around">
                             <li>Git</li>
                             <li>VSCode</li>
                             <li>GitHub</li>
-                            <li>Canva</li>
-                            <li>AWS</li>
                         </ul>
-                    </div>
+                    </div> */}
                 </div>
             </section>
-            <section className="lg:flex justify-around w-full px-2 lg:px-10 gap-10 bg-neutral-200 py-10 items-center">
-                <div className="lg:w-1/3 h-max bg-neutral-700 text-white rounded-lg lg;text-xl p-10 text-center">
+            <section className="flex flex-col-reverse lg:flex-row justify-around w-full lg:px-40 gap-10 bg-neutral-200 lg:py-10 pb-6 items-center">
+                <div className="lg:w-1/3 h-max lg:text-xl py-6 lg:p-20 mt-4 text-center">
                     <p>
-                        My journey has been a bit everywhere, but with every
-                        project I did I had to learn a new technology. The
-                        learning part is my favourite part because it helped me
-                        realize incredible results at whatever work I did.{" "}
-                        <br />
-                        <br />
-                        This project was the first application I did with the
-                        startup during my internship. A no-code, fully visual,
-                        financial startegy, trading app. The whole front was
-                        made entirely by me.
+                        An easy to use trading app with a no-code strategy
+                        creation for Entries and Exits. Includes a drag and drop
+                        system for strategy creation, a chat bot to ask
+                        questions, a save system, and an easy to use interface
+                        for beginners.
                     </p>
-                    <p className="pt-3 text-blue-300 hover:underline">
+                    <p className="text-blue-600 hover:underline mt-4">
                         <a href="https://www.tradeen.ai/" target="_blank">
                             tradeen.Ai
                         </a>
                     </p>
                 </div>
                 <div className="lg:w-1/2 mt-10">
-                    <h3 className="text-center text-4xl">tradeeN.ai</h3>
+                    <h3 className="text-center text-4xl mb-10 hover:underline text-blue-600">
+                        <a href="https://www.tradeen.ai/" target="_blank">
+                            tradeeN.ai
+                        </a>
+                    </h3>
                     <div className="">
                         <Carousel
                             plugins={[
@@ -120,7 +116,7 @@ const Home = () => {
                             ]}
                         >
                             <CarouselContent>
-                                <CarouselItem>
+                                <CarouselItem className="border">
                                     <a
                                         className="bg-white"
                                         href="tradeen_1.png"
@@ -133,7 +129,7 @@ const Home = () => {
                                         />
                                     </a>
                                 </CarouselItem>
-                                <CarouselItem>
+                                <CarouselItem className="border">
                                     <a href="tradeen_2.png" target="_blank">
                                         <img
                                             src="tradeen_2.png"
@@ -141,7 +137,7 @@ const Home = () => {
                                         />
                                     </a>
                                 </CarouselItem>
-                                <CarouselItem>
+                                <CarouselItem className="border">
                                     <a href="tradeen_3.png" target="_blank">
                                         <img
                                             src="tradeen_3.png"
@@ -150,15 +146,19 @@ const Home = () => {
                                     </a>
                                 </CarouselItem>
                             </CarouselContent>
-                            <CarouselPrevious className="lg:visible invisible" />
+                            <CarouselPrevious className="lg:visible invisible " />
                             <CarouselNext className="lg:visible invisible" />
                         </Carousel>
                     </div>
                 </div>
             </section>
-            <section className="lg:flex justify-around w-full px-2 bg-blue-900 text-white lg:px-10 gap-10 py-5 lg:py-10 items-center">
-                <div className="lg:w-1/2">
-                    <h3 className="text-center text-4xl mb-4">Hadaly</h3>
+            <section className="w-full lg:flex justify-around items-center lg:px-40 gap- py-6 lg:py-10 ">
+                <div className="lg:w-1/2 lg:mt-10">
+                    <h3 className="text-center text-4xl mb-10 hover:underline text-blue-600">
+                        <a href="http://www.hadaly.ca" target="_blank">
+                            Hadaly.ca
+                        </a>
+                    </h3>
                     <Carousel
                         plugins={[
                             Autoplay({
@@ -167,12 +167,12 @@ const Home = () => {
                         ]}
                     >
                         <CarouselContent>
-                            <CarouselItem>
+                            <CarouselItem className="border">
                                 <a href="hadaly_1.png" target="_blank">
                                     <img src="hadaly_1.png" alt="Slide 1" />
                                 </a>
                             </CarouselItem>
-                            <CarouselItem>
+                            <CarouselItem className="border">
                                 <a href="hadaly_2.png" target="_blank">
                                     <img
                                         src="hadaly_2.png"
@@ -181,7 +181,7 @@ const Home = () => {
                                     />
                                 </a>
                             </CarouselItem>
-                            <CarouselItem>
+                            <CarouselItem className="border">
                                 <a href="hadaly_3.png" target="_blank">
                                     <img
                                         src="hadaly_3.png"
@@ -191,72 +191,61 @@ const Home = () => {
                                 </a>
                             </CarouselItem>
                         </CarouselContent>
-                        <CarouselPrevious className="lg:visible invisible" />
-                        <CarouselNext className="lg:visible invisible" />
+                        <CarouselPrevious className="lg:visible invisible text-white hover:text-neutral-400" />
+                        <CarouselNext className="lg:visible invisible text-white hover:text-neutral-400" />
                     </Carousel>
                 </div>
-                <div className="lg:w-1/3 h-max bg-blue-950 text-white rounded-lg lg:text-xl p-10 mt-4 text-center">
+                <div className="lg:w-1/3 h-max  rounded-lg lg:text-xl p-10 lg:p-20 mt-4 text-center">
                     <p>
-                        I got hired afterwards as a full stack and worked on a
-                        new app for the same startup. It's an app that
-                        automatizes risk detection for due-diligence. I was
-                        later made lead front-end since there was only one
-                        developer.
-                        <br />
-                        <br />
-                        The application includes a task management for teams, a
-                        complete file system, risk dashboard with data that is
-                        automatized, and a chat bot trained from the data room.
-                        <br />
-                        <span className="text-xs lg:text-sm">
-                            (The information shown is fake for privacy reasons)
-                        </span>
-                        <p className="pt-3 text-blue-300 hover:underline">
-                            <a href="https://www.hadaly.ca" target="_blank">
-                                Hadaly.ca
-                            </a>
-                        </p>
+                        <b>Hadaly</b> is an automated risk detection app to
+                        perfom due-diligence report for M&A's. It includes a
+                        file system for the data room, team and task management,
+                        and dashboards.
+                    </p>
+                    <p className="pt-3 text-blue-600 hover:underline w-max mx-auto">
+                        <a href="https://www.hadaly.ca" target="_blank">
+                            Hadaly.ca
+                        </a>
                     </p>
                 </div>
             </section>
-            <section className="py-10 lg:py-20 w-full flex flex-col lg:flex-row lg:justify-around justify-center bg-black text-white">
+            <section className="py-10 w-full flex flex-col lg:flex-row lg:justify-around justify-center bg-black text-white">
                 <div>
-                    <h3 className="text-2xl px-3 py-1 text-center ">
+                    <h3 className="text-2xl px-3 py-3 text-center ">
                         Let's work together!
                     </h3>
                     <div>
-                        <div className="flex w-full lg:justify-around justify-center gap-3 mt-4">
-                            <div className="hover:bg-neutral-700 w-6 h-6 flex justify-center items-center rounded">
+                        <div className="flex w-full lg:justify-around justify-center gap-10 mt-4">
+                            <div className="hover:text-neutral-400 w-6 h-6 flex justify-center items-center rounded">
                                 <a
                                     className="-mt-1"
                                     href="https://github.com/arielco777"
                                     target="_blank"
                                 >
-                                    <GitHubIcon fontSize="small" />
+                                    <GitHubIcon fontSize="large" />
                                 </a>
                             </div>
-                            <div className="hover:bg-neutral-700 w-6 h-6 flex justify-center items-center rounded">
+                            <div className="hover:text-neutral-400 w-6 h-6 flex justify-center items-center rounded">
                                 <a
                                     className="-mt-1"
                                     href="CV_Ariel-Cohen.pdf"
                                     target="_blank"
                                 >
-                                    <PictureAsPdfIcon fontSize="small" />
+                                    <PictureAsPdfIcon fontSize="large" />
                                 </a>
                             </div>
-                            <div className="hover:bg-neutral-700 w-6 h-6 flex justify-center items-center rounded">
+                            <div className="hover:text-neutral-400 w-6 h-6 flex justify-center items-center rounded">
                                 <a
                                     className="-mt-1"
                                     href="https://www.linkedin.com/in/ariel-martin-cohen/"
                                     target="_blank"
                                 >
-                                    <LinkedInIcon fontSize="small" />
+                                    <LinkedInIcon fontSize="large" />
                                 </a>
                             </div>
                         </div>
                     </div>
                     <p className="text-center mt-4">
-                        Made entirely by{" "}
                         <span className="text-blue-400 font-bold">
                             Ariel Cohen
                         </span>
