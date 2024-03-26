75bca1c4f71b1f4b2ac98fc057e6ab6cfc12e84a
Ariel M. Cohen <arielco777@gmail.com>
2024-03-26 09:28:30 -0400

    again

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
+import { inject } from "@vercel/analytics";
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
