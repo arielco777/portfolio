5a58cfc767468f44e6d6bcd20e61ab1f59b03d9a
Ariel M. Cohen <arielco777@gmail.com>
2024-03-26 09:27:12 -0400

    new analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/react";
 import Home from "./Home";
 
 function App() {
     return (
         <div className="w-full overflow-x-hidden">
             <Home />
+            <Analytics />
         </div>
     );
 }
