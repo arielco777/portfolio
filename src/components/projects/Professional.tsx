1d1c427477d730d1e93109efe8c1e9d8d3616101
Ariel Cohen <arielco777@gmail.com>
2024-06-10 11:48:51 -0400

    a little extra

--- src/components/projects/Professional.tsx
+++ src/components/projects/Professional.tsx
@@ -124,7 +124,8 @@ const Professional = () => {
                         <b>Hadaly</b> is an automated risk detection app to
                         perfom due-diligence report for M&A's. It includes a
                         file system for the data room, team and task management,
-                        and dashboards.
+                        and dashboards. (All information shown are for demo
+                        purposes)
                     </p>
                     <p className="pt-3 text-blue-600 hover:underline w-max mx-auto">
                         <a href="https://www.hadaly.ca" target="_blank">
